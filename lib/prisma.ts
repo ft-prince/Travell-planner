f7785941f32a5566f4c24c821655dcc1f564af37
ft-prince <ft.princeee@gmail.com>
2025-06-28 02:32:47 +0530

    Neon and NextAuth Setup

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/app/generated/prisma/client";
 
 const globalPrisma = globalThis as unknown as { prisma: PrismaClient };
 export const prisma = globalPrisma.prisma || new PrismaClient();
